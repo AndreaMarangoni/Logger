006424c3e54cdf5960e0cccedfb19335852ae69f
Andrea <andy@andy>
2014-06-14 13:42:26 +0100

    template name from Type to Class. Added stream() function

--- src/logger/Logger.h
+++ src/logger/Logger.h
@@ -13,7 +13,7 @@
 #include <cstdio>
 #include <sstream>
 
-template<typename Type>
+template<typename Class>
 class Logger {
 public:
 	enum Level {
@@ -28,6 +28,7 @@ public:
 	inline virtual ~Logger();
 	inline std::ostringstream& get(Level level = NONE);
 	static inline Logger::Level& reportLevel();
+	static inline FILE*& stream();
 
 private:
 	static inline const std::string toString(const Logger::Level& level);
@@ -38,31 +39,38 @@ private:
 
 #endif /* LOGGER_H_ */
 
-template<typename Type>
-typename Logger<Type>::Level& Logger<Type>::reportLevel() {
-	static Logger<Type>::Level reportLevel_ = NONE;
+template<typename Class>
+typename Logger<Class>::Level& Logger<Class>::reportLevel() {
+	static Logger<Class>::Level reportLevel_ = NONE;
 	return reportLevel_;
 }
 
-template<typename Type>
-const std::string Logger<Type>::toString(const Logger::Level& level) {
+template<typename Class>
+FILE*& Logger<Class>::stream(){
+	static FILE* stream_ = stderr;
+	return stream_;
+}
+
+
+template<typename Class>
+const std::string Logger<Class>::toString(const Logger::Level& level) {
 	static std::string names[] = { "CRITICAL", "ERROR", "WARNING", "INFO",
 		"DEBUG", "NONE" };
 	return names[level];
 }
 
-template<typename Type>
-Logger<Type>::Logger() {
+template<typename Class>
+Logger<Class>::Logger() {
 }
 
-template<typename Type>
-Logger<Type>::~Logger() {
+template<typename Class>
+Logger<Class>::~Logger() {
 	os << std::endl;
-	Type::Output(os.str());
+	Class::Output(os.str());
 }
 
-template<typename Type>
-std::ostringstream& Logger<Type>::get(Level level) {
+template<typename Class>
+std::ostringstream& Logger<Class>::get(Level level) {
 	os << "- " << Time().getCurrentTime();
 	os << " " << Logger::toString(level) << ": ";
 	return os;
