14905ef2f3764fc24975107063a98206c56df698
Andrea <andy@andy>
2014-06-14 16:00:53 +0100

    Added MACRO

--- src/logger/LogStdCerr.h
+++ src/logger/LogStdCerr.h
@@ -12,7 +12,13 @@
 
 #include <string>
 
-#define Log(level,message) STDCERRLog().get(level) << message
+#ifndef LOG_MAX_LEVEL
+#define LOG_MAX_LEVEL DEBUG
+#endif
+#define LOG(level,message) \
+  if (level > LOG_MAX_LEVEL) ;\
+  else if (level > Log::ReportingLevel() || !Log::Stream()) ; \
+   else Log().Get(level) << message
 
 class LogStdCerr;
 class STDCERRLog : public Logger<LogStdCerr> {
@@ -21,6 +27,7 @@ class STDCERRLog : public Logger<LogStdCerr> {
 class LogStdCerr{
 public:
 	typedef LogStdCerr Log;
+
 	static void Output(const std::string& message);
 };
 
